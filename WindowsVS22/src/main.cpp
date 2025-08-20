60852ddfc57113751566876455ec4638c7f2964d
KRadzio <rakradek@yahoo.com>
2025-08-20 18:26:02 +0200

    updated the Windows Version

--- WindowsVS22/src/main.cpp
+++ WindowsVS22/src/main.cpp
@@ -1,25 +1,25 @@
 #include "App.hpp"
 
-// using namespace cv;
+using namespace cv;
 
 int main(int argc, char **argv)
 {
-    // if ( argc != 2 )
-    // {
-    //     printf("usage: DisplayImage.out <Image_Path>\n");
-    //     return -1;
-    // }
-    // Mat image;
-    // image = imread( argv[1], 1 );
-    // if ( !image.data )
-    // {
-    //     printf("No image data \n");
-    //     return -1;
-    // }
-    // namedWindow("Display Image", WINDOW_AUTOSIZE );
-    // imshow("Display Image", image);
-    // waitKey(5000);
-    // destroyWindow("Display Image");
+     if ( argc != 2 )
+     {
+         printf("usage: DisplayImage.out <Image_Path>\n");
+         return -1;
+     }
+     Mat image;
+     image = imread( argv[1], 1 );
+     if ( !image.data )
+     {
+         printf("No image data \n");
+         return -1;
+     }
+     namedWindow("Display Image", WINDOW_AUTOSIZE );
+     imshow("Display Image", image);
+     waitKey(5000);
+     destroyWindow("Display Image");
 
    
     int returnCode = App::GetInstance().MainLoop();
